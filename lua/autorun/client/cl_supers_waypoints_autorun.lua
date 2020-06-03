0ae2272b7e218c8c24c7628577a25867d73f15ef
BuildTools <26164930+EmperorSuper@users.noreply.github.com>
2020-06-03 17:25:14 +0300

    Waypoint numbers + Waypoint dots now stick to the side of the screen

--- lua/autorun/client/cl_supers_waypoints_autorun.lua
+++ lua/autorun/client/cl_supers_waypoints_autorun.lua
@@ -4,19 +4,19 @@ if not CLIENT then return end
 
 surface.CreateFont("superswaypoints_WayPointFont", {
 	font = "Orbitron Regular",
-	size = 45,
+	size = 40,
 	antialias = true,
 })
 surface.CreateFont("superswaypoints_WayPointFont_Blur", {
 	font = "Orbitron Regular",
-	size = 45,
+	size = 40,
 	blursize = 4,
 	antialias = true,
 })
 
 surface.CreateFont("superswaypoints_WayPointFont2", {
 	font = "Orbitron Regular",
-	size = 30,
+	size = 25,
 	antialias = true,
 })
 
@@ -34,12 +34,12 @@ surface.CreateFont("superswaypoints_WayPointFont3_Blur", {
 
 surface.CreateFont("superswaypoints_WayPointFont3_NameBeforeOnjective", {
 	font = "Orbitron Regular",
-	size = 26,
+	size = 21,
 	antialias = true,
 })
 surface.CreateFont("superswaypoints_WayPointFont3_NameBeforeOnjective_Blur", {
 	font = "Orbitron Regular",
-	size = 26,
+	size = 21,
 	blursize = 1,
 	antialias = true,
 })
@@ -81,14 +81,29 @@ surface.CreateFont("superswaypoints_WayPointFont3_NameBeforeOnjective_Blur", {
 		end
 	end
 end)]]--
+function draw.Circle( x, y, radius, seg ) -- this is just ripped off the wiki
+	local cir = {}
+
+	table.insert( cir, { x = x, y = y, u = 0.5, v = 0.5 } )
+	for i = 0, seg do
+		local a = math.rad( ( i / seg ) * -360 )
+		table.insert( cir, { x = x + math.sin( a ) * radius, y = y + math.cos( a ) * radius, u = math.sin( a ) / 2 + 0.5, v = math.cos( a ) / 2 + 0.5 } )
+	end
+
+	local a = math.rad( 0 )
+	table.insert( cir, { x = x + math.sin( a ) * radius, y = y + math.cos( a ) * radius, u = math.sin( a ) / 2 + 0.5, v = math.cos( a ) / 2 + 0.5 } )
+
+	surface.DrawPoly( cir )
+end
 
 hook.Remove("PostDrawTranslucentRenderables", "superswaypoints_PostDrawTranslucentRenderables")
 
+local distanceColor = Color(200, 200, 200)
+local numberColor = Color(255, 255, 255)
 hook.Add("HUDPaint", "superswaypoints_HUDPaint", function()
-	for key , value in pairs(ents.GetAll()) do
-		if IsValid(value) and value:GetClass() == "superswaypoints_waypoint" and (value:GetIsWaypointActive() or LocalPlayer():GetMoveType()==8) then
+	for key , value in pairs(ents.FindByClass("superswaypoints_waypoint")) do
+		if IsValid(value) and (value:GetIsWaypointActive() or LocalPlayer():GetMoveType()==8) then
 			local nameColor = value:GetColor()
-			local distanceColor = Color(200, 200, 200)
 			if value:GetIsWaypointActive()==false then
 				if LocalPlayer():GetMoveType()==8 then
 					nameColor = Color(value:GetColor().r, value:GetColor().g, value:GetColor().b, 125)
@@ -96,32 +111,46 @@ hook.Add("HUDPaint", "superswaypoints_HUDPaint", function()
 				else
 					return
 				end
+			else
+				distanceColor = Color(200, 200, 200)
 			end
 			local screenpos = value:GetPos():ToScreen()
-			if value.GetObjectiveBeforeName and value:GetObjectiveBeforeName() then
-				draw.SimpleText(value:GetWaypointObjective(),"superswaypoints_WayPointFont3",screenpos.x,screenpos.y-25,distanceColor,1,1)
-				if value:GetShouldDrawBlur() then
-					draw.SimpleText(value:GetWaypointObjective(),"superswaypoints_WayPointFont3_Blur",screenpos.x,screenpos.y-25,distanceColor,1,1)
-				end
+			if (screenpos.x < (ScrW()/2 + 150) and screenpos.x > (ScrW()/2 - 150)) and (screenpos.y < (ScrH()/2 + 150) and screenpos.y > (ScrH()/2 - 150)) then
+				if value.GetObjectiveBeforeName and value:GetObjectiveBeforeName() then
+					draw.SimpleText(value:GetWaypointObjective(),"superswaypoints_WayPointFont3",screenpos.x,screenpos.y-25,distanceColor,1,1)
+					if value:GetShouldDrawBlur() then
+						draw.SimpleText(value:GetWaypointObjective(),"superswaypoints_WayPointFont3_Blur",screenpos.x,screenpos.y-25,distanceColor,1,1)
+					end
 
-				draw.SimpleText(value:GetWaypointName(),"superswaypoints_WayPointFont",screenpos.x,screenpos.y,nameColor,1,1)
-				if value:GetShouldDrawBlur() then
-					draw.SimpleText(value:GetWaypointName(),"superswaypoints_WayPointFont_Blur",screenpos.x,screenpos.y,nameColor,1,1)
-				end
+					draw.SimpleText(value:GetWaypointName(),"superswaypoints_WayPointFont",screenpos.x,screenpos.y,nameColor,1,1)
+					if value:GetShouldDrawBlur() then
+						draw.SimpleText(value:GetWaypointName(),"superswaypoints_WayPointFont_Blur",screenpos.x,screenpos.y,nameColor,1,1)
+					end
 
-				draw.SimpleText("Distance: " .. math.Round(LocalPlayer():GetPos():Distance(value:GetPos())/50) .. "m","superswaypoints_WayPointFont2",screenpos.x,screenpos.y + 30,distanceColor,1,1)
+					draw.SimpleText("Distance: " .. math.Round(LocalPlayer():GetPos():Distance(value:GetPos())/50) .. "m","superswaypoints_WayPointFont2",screenpos.x,screenpos.y + 25,distanceColor,1,1)
+				else
+					draw.SimpleText(value:GetWaypointObjective(),"superswaypoints_WayPointFont3_NameBeforeOnjective",screenpos.x,screenpos.y,distanceColor,1,1)
+					if value:GetShouldDrawBlur() then
+						draw.SimpleText(value:GetWaypointObjective(),"superswaypoints_WayPointFont3_NameBeforeOnjective_Blur",screenpos.x,screenpos.y,distanceColor,1,1)
+					end
+
+					draw.SimpleText(value:GetWaypointName(),"superswaypoints_WayPointFont",screenpos.x,screenpos.y-21,nameColor,1,1)
+					if value:GetShouldDrawBlur() then
+						draw.SimpleText(value:GetWaypointName(),"superswaypoints_WayPointFont_Blur",screenpos.x,screenpos.y-21,nameColor,1,1)
+					end
+
+					draw.SimpleText("Distance: " .. math.Round(LocalPlayer():GetPos():Distance(value:GetPos())/50) .. "m","superswaypoints_WayPointFont2",screenpos.x,screenpos.y + 16,distanceColor,1,1)
+				end
 			else
-				draw.SimpleText(value:GetWaypointObjective(),"superswaypoints_WayPointFont3_NameBeforeOnjective",screenpos.x,screenpos.y,distanceColor,1,1)
-				if value:GetShouldDrawBlur() then
-					draw.SimpleText(value:GetWaypointObjective(),"superswaypoints_WayPointFont3_NameBeforeOnjective_Blur",screenpos.x,screenpos.y,distanceColor,1,1)
+				surface.SetDrawColor(nameColor)
+				draw.NoTexture()
+				print(value:GetWaypointNumber())
+				if value:GetWaypointNumber() == "" then
+					draw.Circle(math.Clamp(screenpos.x, 20, ScrW()-20), math.Clamp(screenpos.y, 20, ScrH()-20), 20, 20)
+				else
+					draw.Circle(math.Clamp(screenpos.x, 25, ScrW()-25), math.Clamp(screenpos.y, 25, ScrH()-25), 25, 20)
+					draw.SimpleText(value:GetWaypointNumber(),"superswaypoints_WayPointFont",math.Clamp(screenpos.x, 25, ScrW()-25), math.Clamp(screenpos.y, 25, ScrH()-25)-1,numberColor,1,1)
 				end
-
-				draw.SimpleText(value:GetWaypointName(),"superswaypoints_WayPointFont",screenpos.x,screenpos.y-26,nameColor,1,1)
-				if value:GetShouldDrawBlur() then
-					draw.SimpleText(value:GetWaypointName(),"superswaypoints_WayPointFont_Blur",screenpos.x,screenpos.y-26,nameColor,1,1)
-				end
-
-				draw.SimpleText("Distance: " .. math.Round(LocalPlayer():GetPos():Distance(value:GetPos())/50) .. "m","superswaypoints_WayPointFont2",screenpos.x,screenpos.y + 21,distanceColor,1,1)
 			end
 		end
 	end
--- lua/entities/superswaypoints_waypoint.lua
+++ lua/entities/superswaypoints_waypoint.lua
@@ -11,15 +11,17 @@ end
 function ENT:SetupDataTables()
 	self:NetworkVar("String",1,"WaypointName", {KeyName = "waypoint_name", Edit = {type = "Generic", order = 1}})
 	self:NetworkVar("String",2,"WaypointObjective", {KeyName = "waypoint_objective", Edit = {type = "Generic", order = 2}})
-	self:NetworkVar("Bool",3,"IsWaypointActive", {KeyName = "is_waypoint_active", Edit = {type = "Boolean", order = 3}})
-	self:NetworkVar("Bool",4,"ShouldDrawBlur", {KeyName = "ShouldDrawBlur", Edit = {type = "Boolean", order = 4}})
-	self:NetworkVar("Bool",5,"ObjectiveBeforeName", {KeyName = "ObjectiveBeforeName", Edit = {type = "Boolean", order = 5}})
+	self:NetworkVar("String",3,"WaypointNumber", {KeyName = "waypoint_number", Edit = {type = "Generic", order = 3}})
+	self:NetworkVar("Bool",4,"IsWaypointActive", {KeyName = "is_waypoint_active", Edit = {type = "Boolean", order = 4}})
+	self:NetworkVar("Bool",5,"ShouldDrawBlur", {KeyName = "ShouldDrawBlur", Edit = {type = "Boolean", order = 5}})
+	self:NetworkVar("Bool",6,"ObjectiveBeforeName", {KeyName = "ObjectiveBeforeName", Edit = {type = "Boolean", order = 6}})
 	if SERVER then
 		self:SetWaypointName("WAYPOINT NAME")
 		self:SetWaypointObjective("")
 		self:SetIsWaypointActive(false)
 		self:SetShouldDrawBlur(false)
 		self:SetObjectiveBeforeName(true)
+		self:SetWaypointNumber("")
 	end
 end
 
